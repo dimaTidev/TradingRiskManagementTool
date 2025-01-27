2a831dbdcf1c5e968b10217c9f4904be8f6daf52
Dima <81911982+dimaTidev@users.noreply.github.com>
2025-01-27 01:39:24 -0500

    Fixed settings style

--- src/app/Panel/panelSettings.jsx
+++ src/app/Panel/panelSettings.jsx
@@ -7,7 +7,7 @@ import AccountList, { AccountCreationButton } from "./Accounts/accountList";
 export default function Settings({onClose, isAdvancedMode}) {
     return (
       <div className={Styles.settingsOverlay}>
-          <div className={Styles.panel} onClick={(e) => e.stopPropagation()}>
+          <div className={Styles.base} onClick={(e) => e.stopPropagation()}>
               {/* <div className={Styles.header}>
                   <a className={Styles.headerFont}>Settings</a>
                   <ButtonIcon src="close.svg" quiet={true} size={Size.L} onClick={onClose}/>
